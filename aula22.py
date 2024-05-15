2bc29abd7de2a6d7806e2cbeb1219b847aa08bf6
Luan <azambuja.luanz@gmail.com>
2024-05-15 16:12:36 -0300

    att1

---
+++ aula22.py
@@ -0,0 +1,19 @@
+"""
+entrada = input('[E]ntrar [S]air:')
+senha_digitada = input('Senha: ')
+
+
+senha_permitida = '123456'
+# if true:
+#
+if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
+    print('Entrar')
+else:
+    print('Sair')
+"""
+
+# Avaliação de curto circuito
+#print(0 or False or 0 or 'abc' or True)
+
+senha = input('Senha: ') or 'Sem senha'
+print(senha)
---
+++ aula23.py
@@ -0,0 +1,9 @@
+# Operador Lógico "Not"
+# Usado para inverter expressões
+# not true = False
+# not false = True
+
+#senha = input('Senha: ')
+
+print(not True)
+print(not False)
