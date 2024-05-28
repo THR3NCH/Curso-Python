6787416f7559402dfafe98f4bf6603ab63816705
Luan <azambuja.luanz@gmail.com>
2024-05-28 14:34:26 -0300

    att1

---
+++ aula26.py
@@ -0,0 +1,15 @@
+"""
+Formatação Basica de strings
+s - string
+d - int
+f - float
+.<numero de digitos>f
+x ou X - hexadecimal
+
+"""
+
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}')
+print(f'{variavel: ^10}')
---
+++ aula27.py
@@ -0,0 +1,3 @@
+
+variavel = 'Olá mundo'
+print(variavel[4:7])
